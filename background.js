e92ee3737adc36c924f60bf64542da88f532789e
Brian Sierakowski <briansierakowski@gmail.com>
2023-10-28 17:43:46 -0500

    Display a message when the tab is slept

--- background.js
+++ background.js
@@ -14,12 +14,22 @@ function scheduleTabSleep(tabInfo) {
     chrome.storage.local.set({ sleptTabs });
   });
 
-  // Closing the tab
   chrome.tabs.remove(tabId);
-
-  // Setting the alarm to check every minute for tabs to reopen
   chrome.alarms.create('checkTabs', { periodInMinutes: 1 });
+
+  // Open a new tab with a message
+  chrome.tabs.create({
+    url: 'data:text/html,<html><body><h1>Your tab has been slept!</h1><p>It will wake up at: ' +
+      new Date(wakeupTime).toLocaleString() +
+      '</p><p>This tab will close shortly.</p></body></html>'
+  }, function (tab) {
+    // Close the message tab after 3 seconds
+    setTimeout(() => {
+      chrome.tabs.remove(tab.id);
+    }, 3000);
+  });
 }
+
 
 chrome.alarms.onAlarm.addListener(alarm => {
   if (alarm.name === 'checkTabs') {
