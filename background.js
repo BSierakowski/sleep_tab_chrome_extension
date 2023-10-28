550e94a918d62d9da042d25541a86d7ba2679b5a
Brian Sierakowski <briansierakowski@gmail.com>
2023-10-28 17:07:34 -0500

    closing tab and adding to list, but name isn't showing up correctly

--- background.js
+++ background.js
@@ -1,24 +1,22 @@
-function scheduleTabSleep(wakeupTime, tabId) {
-  const alarmInfo = {
-    when: wakeupTime
-  };
-
-  chrome.alarms.create(`tab_${tabId}`, alarmInfo);
-  chrome.tabs.remove(tabId);
-}
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'scheduleTabSleep') {
-    scheduleTabSleep(request.wakeupTime, request.tabId);
+    scheduleTabSleep(request);
     sendResponse({ result: 'Tab scheduled successfully' });
   }
 });
 
-chrome.alarms.onAlarm.addListener(alarm => {
-  chrome.notifications.create({
-    type: 'basic',
-    iconUrl: 'images/icon128.png',
-    title: 'Tab Wake Up!',
-    message: 'It\'s time to reopen the tab you slept.'
+function scheduleTabSleep(tabInfo) {
+  const { wakeupTime, tabId, tabUrl, tabTitle } = tabInfo;
+
+  chrome.storage.local.get('sleptTabs', function (data) {
+    let sleptTabs = data.sleptTabs || [];
+    sleptTabs.push({ wakeupTime, tabId, tabUrl, tabTitle });
+    chrome.storage.local.set({ sleptTabs });
   });
-});
+
+  const alarmInfo = {
+    when: wakeupTime
+  };
+  chrome.alarms.create(`tab_${tabId}`, alarmInfo);
+  chrome.tabs.remove(tabId);
+}
--- popup.js
+++ popup.js
@@ -10,12 +10,32 @@ document.getElementById('sleepForm').addEventListener('submit', function (e) {
       chrome.runtime.sendMessage({
         action: 'scheduleTabSleep',
         wakeupTime: wakeupTime,
-        tabId: tab.id
+        tabId: tab.id,
+        tabUrl: tab.url,
+        tabTitle: tab.title
       }, response => {
         console.log(response.result);
+        loadSleptTabs();
       });
     });
   } else {
     alert('Please enter a future time.');
   }
 });
+
+function loadSleptTabs() {
+  chrome.storage.local.get('sleptTabs', function (data) {
+    const sleptTabsList = document.getElementById('sleptTabsList');
+    sleptTabsList.innerHTML = '';
+
+    const sleptTabs = data.sleptTabs || [];
+    sleptTabs.forEach(tabInfo => {
+      const li = document.createElement('li');
+      li.textContent = `${tabInfo.tabTitle} - Wake up at: ${new Date(tabInfo.wakeupTime).toLocaleString()}`;
+      sleptTabsList.appendChild(li);
+    });
+  });
+}
+
+// Load the slept tabs when the popup is opened
+loadSleptTabs();
